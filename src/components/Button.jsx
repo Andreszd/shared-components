e9341af05427350bae314851f578c41dc66f683d
Jose Andres <contretaa@gmail.com>
2024-04-17 13:45:39 -0400

    add mui
    Signed-off-by: Jose Andres <contretaa@gmail.com>

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,8 @@
+import { Button as MuiButton } from "@mui/material";
+
 export const Button = () => {
-  return <button>dev button v3</button>;
+  return <MuiButton>mui dev button v3</MuiButton>;
+  // return <button>dev button v3</button>;
 };
 
 Button.propTypes = {};
